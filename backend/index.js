abcf09abdc0f5dc3c3066719c4b6c326f32e3aca
Hayatzendah <hyatzndah259@gmail.com>
2025-12-09 19:34:54 +0200

    Setup postgreSQL connection and test DB connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,14 @@
 const app = require("./app");
 const PORT = app.get("port");
+const pool = require('./connection');
+
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('❌ Database error:', err);
+  } else {
+    console.log('✅ DB Time:', res.rows[0]);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
