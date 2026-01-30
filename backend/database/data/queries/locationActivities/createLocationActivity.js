cb5230183787a9e7dec76006337987ca4997e634
AlaaSaadeddin <alaa.swaireh@gmail.com>
2026-01-30 18:31:33 +0200

    Activity: Change project selection to text input and fix image display

--- backend/database/data/queries/locationActivities/createLocationActivity.js
+++ backend/database/data/queries/locationActivities/createLocationActivity.js
@@ -1,8 +1,8 @@
 // Note: This assumes activities table has location_id, start_date, end_date fields
 // If schema differs, it may need to be updated
 const createLocationActivityQuery = `
-  INSERT INTO activities (name, activity_type, employee_id, location_id, start_date, end_date, activity_days, status, description)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+  INSERT INTO activities (name, activity_type, employee_id, location_id, start_date, end_date, activity_days, status, description, project_id, images)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
   RETURNING *;
 `;
 
