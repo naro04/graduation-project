66e0f670d33a2fffe5b3d0390556ce86c29c7c74
AlaaSaadeddin <alaa.swaireh@gmail.com>
2026-03-08 14:10:12 +0200

    fix: join locations with location_types for type_name

--- backend/database/data/queries/locationAssignments/getLocationAssignments.js
+++ backend/database/data/queries/locationAssignments/getLocationAssignments.js
@@ -5,10 +5,11 @@ const getLocationAssignmentsQuery = `
     e.employee_code,
     el.location_id,
     l.name as location_name,
-    l.location_type
+    lt.name as type_name
   FROM employee_locations el
   JOIN employees e ON el.employee_id = e.id
   JOIN locations l ON el.location_id = l.id
+  LEFT JOIN location_types lt ON l.location_type = lt.id::text OR l.location_type = lt.name
   WHERE ($1::UUID IS NULL OR el.employee_id = $1);
 `;
 
